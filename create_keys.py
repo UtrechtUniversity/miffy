417a74a9f664ee1f548f81adbd4f8a86f010e73a
RVoor <63646486+RVoor@users.noreply.github.com>
2020-07-17 16:53:16 +0200

    Update anonymizing script

--- create_keys.py
+++ create_keys.py
@@ -3,10 +3,7 @@ import json
 import re
 import pandas as pd
 import string
-import time
-import progressbar
 import logging
-
 
 class CreateKeys:
     """ Detect personal sensitive information in text; create keyfile for user and person names"""
@@ -60,34 +57,6 @@ class CreateKeys:
 
         return ''.join(new)
 
-    # def read_json(self, files):
-    #     """ Reads .JSON files as list """
-    #
-    #     alldata = {}
-    #     for file in files:
-    #         with open(file, encoding="utf8") as json_file:
-    #             data = json.load(json_file)
-    #             if len(data) > 0:
-    #                 alldata.update({f'{file}': data})
-    #
-    #     return alldata
-
-    def extr_profile(self, df):
-        """Extract all profile information from entered file """
-
-        dictionary = {}
-        for file in df:
-            if file.find('profile') >= 0:
-                for key in df[file]:
-                    if key == 'biography':
-                        dictionary.update({df[file][key]: '__biography'})
-                    if key.find('facebook') >= 0:
-                        dictionary.update({df[file][key]: '__facebookinfo'})
-                    if key.find('birth') >= 0:
-                        dictionary.update({df[file][key]: '__birthdate'})
-
-        return dictionary
-
     def read_participants(self):
         """ Open file with all participant numbers """
 
@@ -98,117 +67,124 @@ class CreateKeys:
 
         return participants
 
+    def extr_profile(self, df):
+        """Extract all profile information from entered file """
+
+        try:
+            df_profile = df[list(df.columns[df.columns.str.contains(pat='facebook|birth|email')])].dropna(how='all')
+        except ValueError:
+            newList = [i for i in list(df.columns) if not isinstance(i, int)]
+            df = df[newList]
+            df_profile = df[list(df.columns[df.columns.str.contains(pat='facebook|birth|email')])].dropna(how='all')
+
+        dictionary = {}
+        for file in df_profile.columns:
+            if file.find('mail') >= 0:
+                dictionary.update({df_profile[file][0]: '__email'})
+            elif (file.find('facebook') >= 0) and (file.find('birth') < 0):
+                dictionary.update({df_profile[file][0]: '__facebookinfo'})
+            elif file.find('birth') >= 0:
+                dictionary.update({df_profile[file][0]: '__birthdate'})
+
+        return dictionary
+
     def extr_usernames(self, df):
         """Extract all usernames in entered file """
 
-        # Search for columns with usernames in them
         username = []
-        for file in df:
-            if type(df[file]) == dict:
-                for key in df[file].keys():
-                    if key.find('like') >= 0:
-                        for n in range(len(df[file][key])):
-                            found = df[file][key][n][1]
-                            if found not in username:
-                                username.append(found)
-                    if key.find('follow') >= 0 or key.find('friend') >= 0:
-                        founds = list(df[file][key].keys())
-                        for found in founds:
-                            if found not in username:
-                                username.append(found)
-                    if key.find('user') >= 0:
-                        if len(df[file][key]) > 1 and type(df[file][key]) is not str:
-                            founds = list(df[file][key].keys())
-                            for found in founds:
-                                if found not in username:
-                                    username.append(found)
-                    if key.find('comment') >= 0:
-                        if key != 'allow_comments_from':
-                            for n in range(len(df[file][key])):
-                                found = df[file][key][n][-1]
-                                if found not in username:
-                                    username.append(found)
-                    if key.find('saved') >= 0 or key.find('polls') >= 0:
-                        founds = df[file][key]
-                        for found in founds:
-                            name = found[1]
-                            if name not in username:
-                                username.append(name)
 
-        # Search for nested usernames
-        found = []
-        regex = [re.compile("\\'author\\': \\'(.*?)\\'"), re.compile("\\'search_click\\': \\'(.*?)\\'"),
-                 re.compile("\\'username\\': \\'(.*?)\\'"), re.compile("\\'media_owner\\': \\'(.*?)\\'"),
-                 re.compile(r"{'participants': \[(.*?)],")]
-        for reg in regex:
-            found.append(re.findall(reg, str(df)))
+        try:
+            df_users = df[list(df.columns[df.columns.str.contains(pat='user|follow|friends')])].dropna(how='all')
+        except ValueError:
+            newList = [i for i in list(df.columns) if not isinstance(i, int)]
+            df = df[newList]
+            df_users = df[list(df.columns[df.columns.str.contains(pat='user|follow|friends')])].dropna(how='all')
 
-        for index in found:
-            if type(index) == list:
-                for item in index:
-                    found_item = item.split(",")
-                    for sender in found_item:
-                        sender = sender.replace("'", "")
-                        if sender.find(" ") == 0:
-                            sender = sender.replace(" ", "")
-                        if sender not in username and sender != 'Username unavailable.':
-                            username.append(sender)
-            elif index not in username and index != 'Username unavailable.':
-                username.append(index)
+        df_search = df[list(df.columns[df.columns.str.contains(pat='search')])].dropna(how='all')
+        df_seen = df[list(df.columns[df.columns.str.contains(pat='seen|watched')])].dropna(how='all')
+        df_saved = df[list(df.columns[df.columns.str.contains(pat='saved')])].dropna(how='all')
+        df_likes = df[list(df.columns[df.columns.str.contains(pat='like')])].dropna(how='all')
+        df_comments = df[list(df.columns[df.columns.str.contains(pat='_comment')])].dropna(how='all')
+
+        username.extend(list(df_users.index))
+
+        for col in df_search.columns:
+            try:
+                username.extend(list(df_search[col]))
+            except:
+                next
+
+        for col in df_seen.columns:
+            try:
+                for idx, row in df_seen[col].iteritems():
+                    if pd.isna(row):
+                        next
+                    else:
+                        try:
+                            username.append(row['author'])
+                        except KeyError:
+                            username.append(row['username'])
+            except:
+                next
+
+        try:
+            df_messages = df['participants'].dropna(how='all')
+            username.extend([j for i in list(df_messages) for j in i])
+        except:
+            next
+
+        for col in df_saved.columns:
+            try:
+                username.extend([item[1] for item in list(df_saved[col])])
+            except:
+                next
+
+        for col in df_likes.columns:
+            try:
+                username.extend([item[1] for item in list(df_likes[col].dropna(how = 'all'))])
+            except:
+                next
+
+        for col in df_comments.columns:
+            try:
+                username.extend([item[2] for item in list(df_comments[col].dropna(how = 'all'))])
+            except:
+                next
 
         # Create dictionary with original username and mingled substitute
         participants = self.read_participants()
-        dictionary = {}
-        for name in username:
-            col = list(participants.columns)
-            if name not in list(participants[col[0]]) and name.lower() is not 'instagram':
-                dictionary.update({name: self.mingle(name)})
-            else:
-                dictionary.update({name: list(participants[col[1]][participants[col[0]] == name])[0]})
+        participants = participants.set_index(participants.columns[0])
+        dictionary = participants.to_dict()[participants.columns[0]]
+
+        for name in set(username):
+            try:
+                if name not in dictionary and name.lower() is not 'instagram':
+                    dictionary.update({name: self.mingle(name)})
+            except AttributeError:
+                next
 
         return dictionary
 
     def extr_names(self, df):
         """Extract all names in entered file """
 
-        # Search for columns with names in them
         names = []
-        for file in df:
-            if type(df[file]) != dict and len(df[file]) > 1:
-                for n in range(len(df[file])):
-                    for key in df[file][n].keys():
-                        if key.find('_name') >= 0:
-                            found = df[file][n][key]
-                            if len(found) > 1:
-                                names.append(found)
-            elif type(df[file]) == dict:
-                for key in df[file].keys():
-                    if key.find('name') == 0:
-                        names.append(df[file][key])
-                    if type(df[file][key]) == dict:
-                        for i in df[file][key].keys():
-                            if i.find('name') >= 0:
-                                for n in range(len(df[file][key][i])):
-                                    found = df[file][key][i][n]
-                                    if len(found) > 1:
-                                        names.append(found)
+        try:
+            names.extend(list(df['name'].dropna(how='all')))
+        except:
+            next
 
-        # Search for nested names
-        regex = re.compile("\\'name\\': \\'(.*?)\\'")
-        found = re.findall(regex, str(df))
+        try:
+            names.extend(df['registration_info'].dropna(how='all')['registration_username'])
+        except:
+            next
 
-        for index in found:
-            if type(index) == list:
-                for item in index:
-                    found_item = item.split(",")
-                    for sender in found_item:
-                        sender = sender.replace("'", "")
-                        if sender.find(" ") == 0:
-                            sender = sender.replace(" ", "")
-                        if sender not in names and sender != 'Username unavailable.':
-                            names.append(sender)
-            elif index not in names and index != 'Username unavailable.':
-                names.append(index)
+        try:
+            df_names = df[list(df.columns[df.columns.str.contains(pat='t_name')])].dropna(how='all')
+            for col in df_names.columns:
+                names.extend(list(df_names[col]))
+        except:
+            next
 
         # Search for (the most) common names in saved usernames
         path = Path(self.input_folder) / 'Firstnames_NL.lst'
@@ -220,13 +196,13 @@ class CreateKeys:
                 firstnames.append(i)
 
         for name in firstnames:
-            if name.lower() in str(df).lower():
+            if name.lower() in json.dumps(df.to_dict(orient='list')).lower():
                 if name not in names or name.lower() not in names:
                     names.append(name)
 
         # Create dictionary with original name and mingled substitute
         dictionary = {}
-        for name in names:
+        for name in set(names):
             dictionary.update({name: self.mingle(name)})
 
         return dictionary
@@ -234,14 +210,19 @@ class CreateKeys:
     def extr_mail(self, df):
         """Extract all email adresses in entered file """
 
+        mails = []
+        try:
+            mails.extend(list(df['email'].dropna(how='all')))
+        except:
+            next
+
         # Search for nested email adresses
         regex = re.compile(("([a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`"
                             "{|}~-]+)*(@|\sat\s)(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?(\.|"
                             "\sdot\s))+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)"))
 
-        mail_text = re.findall(regex, str(df))
+        mail_text = re.findall(regex, json.dumps(df.to_dict(orient='list')))
 
-        mails = []
         for i in range(len(mail_text)):
             found = mail_text[i][0].replace("'", "")
             if found.find('@') > 1 and found not in mails:
@@ -249,7 +230,7 @@ class CreateKeys:
 
         # Create dictionary with original email adress and mingled substitute
         dictionary = {}
-        for mail in mails:
+        for mail in set(mails):
             dictionary.update({mail: '__emailadress'})
 
         return dictionary
@@ -257,27 +238,31 @@ class CreateKeys:
     def extr_phone(self, df):
         """Extract all phone numbers in entered file """
 
-        # Search for columns with phone numbers
         number = []
-        for file in df:
-            if type(df[file]) != dict and len(df[file]) > 1:
-                for n in range(len(df[file])):
-                    for key in df[file][n].keys():
-                        if key.find('contact') >= 0:
-                            num = df[file][n][key]
-                            if num.find('@') < 0:
-                                number.append(num)
+        try:
+            df_phone = df[list(df.columns[df.columns.str.contains(pat='contact')])].dropna(how='all')
+            for col in df_phone.columns:
+                if df_phone[col].str.contains(re.compile("[0-9]")).any():
+                    for i in df_phone[col].index:
+                        try:
+                            number.append(int(df_phone[col][i]))
+                        except:
+                            next
+                else:
+                    next
+        except:
+            next
 
         # Search for nested phone numbers
         regex = [re.compile("(?<!\d)\d{9,10}(?!\d)"), re.compile("[0-9]{2}\-[0-9]{8}")]
         for reg in regex:
-            for i in re.findall(reg, str(df)):
+            for i in re.findall(reg, json.dumps(df.to_dict(orient='list'))):
                 if i not in number:
                     number.append(i)
 
         # Create dictionary with original phone number and mingled substitute
         dictionary = {}
-        for num in number:
+        for num in set(number):
             dictionary.update({num: '__phonenumber'})
 
         return dictionary
@@ -285,27 +270,32 @@ class CreateKeys:
     def extr_http(self, df):
         """Extract all URLs in entered file """
 
-        df = json.dumps(df)
-
         regex = re.compile("(?P<url>https?:\/\/[^\s]+)")
-        https = re.findall(regex, df)
+        https = re.findall(regex, json.dumps(df.to_dict(orient='list')))
 
         dictionary = {}
-        for http in https:
+        for http in set(https):
             while http[-1] in string.punctuation:
                 http = http[:-1]
             dictionary.update({http: '__url'})
 
         return dictionary
 
-    def replace_info(self, df):
+    def replace_info(self):
         """Replace sensitive info that Anonymize can't replace """
 
-        file = json.dumps(df)
+        dic = {}
+        for json_file in self.data_package.glob('*.json'):
+            with open(json_file, encoding="utf8") as f:
+                data = json.load(f)
+                if len(data) > 0:
+                    dic.update({f'{json_file}': data})
+
+        file = json.dumps(dic)
 
         # Replace bio and gender info
-        bio = re.findall(re.compile("\"biography\"\: \"(.*?)\""), file)
-        gender = re.findall(re.compile("\"gender\"\: \"(.*?)\""), file)
+        bio = re.findall(re.compile("biography\"\: \"(.*?)\""), file)
+        gender = re.findall(re.compile("gender\"\: \"(.*?)\""), file)
 
         if len(bio) >= 1:
             file = file.replace(bio[0], '__bio')
@@ -323,32 +313,34 @@ class CreateKeys:
     def create_keys(self):
         """Extract all sensitive information from files in given folder """
 
-        df = {}
-        for json_file in self.data_package.glob('*.json'):
-            with open(json_file, encoding="utf8") as f:
-                data = json.load(f)
-                if len(data) > 0:
-                    df.update({f'{json_file}': data})
+        json_files = self.data_package.glob('*.json')
+        df = pd.DataFrame()
+        for json_file in json_files:
+            try:
+                my_df = pd.read_json(json_file)
+                df = df.append(my_df, sort=False)
+            except ValueError:
+                with open(json_file, encoding="utf8") as f:
+                    data = json.load(f)
+                my_df = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in data.items()]))
+                df = df.append(my_df, sort=False)
 
         # Create key file
         self.logger.info(f"Creating key file for {self.data_package}...")
         dictionary = self.extr_usernames(df)
         functions = [self.extr_profile, self.extr_names, self.extr_mail, self.extr_phone, self.extr_http]
 
-        widgets = [progressbar.Percentage(), progressbar.Bar()]
-        bar = progressbar.ProgressBar(widgets=widgets, max_value=len(functions)).start()
-        for index, function in enumerate(functions):
-            dictionary.update(function(df))
-            time.sleep(0.1)
-            bar.update(index + 1)
+        for function in functions:
+            try:
+                dictionary.update(function(df))
+            except TypeError:
+                next
 
-        bar.finish()
-
-        self.replace_info(df)
+        self.replace_info()
 
         dic = pd.DataFrame(list(dictionary.items()))
         dic = dic.rename(columns={0: 'id', 1: 'subt'})
 
-        subt = dictionary[f'{self.data_package.stem}']
-        export_path = Path(self.output_folder, 'keys' + f"_{subt}.csv")
+        subt = dictionary[f'{self.data_package.name}']
+        export_path = Path(self.input_folder, 'keys' + f"_{subt}.csv")
         dic.to_csv(export_path, index=False, encoding='utf-8')
