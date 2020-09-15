d330ab64bf4df29965f219f6607007fefef3adbf
vos00076 <m.g.devos@uu.nl>
2020-09-15 13:23:06 +0200

    include parsing of email, phone; output as dictionary

--- parse_json.py
+++ parse_json.py
@@ -5,6 +5,7 @@ import dateutil.parser
 from datetime import datetime
 import json
 import logging
+import pandas as pd
 from pathlib import Path
 import re
 
@@ -14,48 +15,50 @@ class ParseJson:
 
     def __init__(self, data: dict):
         self.logger = logging.getLogger('anonymizing.parse_json')
-        self.usr = r'^\S{6,}$'
+        self.email = r'[\w\.-]+@[\w\.-]+'
         self.data = data
         self.labels = self.get_labels()
 
     def json_extract_usrs(self) -> set:
         """Extract user names from nested JSON."""
 
-        arr = []
+        key_dict = {}
 
         # Extract values with recursive function
-        values = self.extract(self.data, arr)
-        flat_values = ParseJson.format_list(values)
+        keys = self.extract(self.data, key_dict)
 
-        return flat_values
+        return keys
 
-    def extract(self, obj, arr: list) -> list:
+    def extract(self, obj, key_dict: dict) -> dict:
         """Recursively search for values of key in JSON tree."""
 
         if isinstance(obj, dict):
             for k, v in obj.items():
                 if v:
                     if isinstance(v, (dict, list)):
-                        self.extract(v, arr)
+                        self.extract(v, key_dict)
                     elif isinstance(v, str):
+                        # If the key matches predefined labels, value may contain sensitive info
                         if any(label.match(k) for label in self.labels):
-                            if re.match(self.usr, v):
-                                try:
-                                   int(v)
-                                   pass
-                                except ValueError:
-                                    arr.append(v)
-                        elif re.match(self.usr, k) and self.check_datetime(v):
-                            arr.append(k)
+                            if re.match(self.email, v):
+                                key_dict[v] = '__emailaddress'
+                            elif self.check_phone(v):
+                                key_dict[v] = '__phonenumber'
+                            elif self.check_name(v):
+                                key_dict[v] = '__name'
+                        elif self.check_name(k) and self.check_datetime(v):
+                            key_dict[k] = '__name'
         elif isinstance(obj, list):
             if obj:
                 try:
                     names = self.get_username(obj)
-                    arr.append(names)
+                    for name in names:
+                        key_dict[name] = '__name'
                 except:
                     for item in obj:
-                        self.extract(item, arr)
-        return arr
+                        self.extract(item, key_dict)
+
+        return key_dict
 
     def get_labels(self) -> list:
         """Get regular expressions of json search labels"""
@@ -75,15 +78,38 @@ class ParseJson:
 
         return regex_labels
 
-    def check_datetime(self,date_text: str) -> datetime:
+    def check_name(self, text: str):
+        """check if given string is valid username"""
+
+        name = r'^\S{6,}$'
+
+        try:
+            int(text)
+            return None
+        except:
+            if re.match(name, text):
+                return text
+
+    def check_phone(self,text: str):
+        """check if given string is valid phone nr"""
+
+        patterns = [r'(?<!\d)\d{9,10}(?!\d)',
+                 r'[0-9]{2}\-[0-9]{8}']
+
+        phone_nrs = [re.compile(p) for p in patterns]
+
+        if any(nr.match(text) for nr in phone_nrs):
+            return text
+
+    def check_datetime(self,text: str) -> datetime:
         """Check if given string can be converted to a datetime format"""
         try:
-            res = dateutil.parser.parse(date_text)
+            res = dateutil.parser.parse(text)
             return res
         except ValueError:
             pass
         try:
-            res = datetime.utcfromtimestamp(int(date_text))
+            res = datetime.utcfromtimestamp(int(text))
             return res
         except ValueError:
             pass
@@ -105,6 +131,16 @@ class ParseJson:
                         pass
 
         return usr_list
+
+    @staticmethod
+    def format_dict(obj:list)-> dict:
+        """Format irregular list of dictionaries and remove duplicates"""
+
+        no_dupl = [i for n, i in enumerate(obj) if i not in obj[n + 1:]]
+
+        new_dict = {k: v for d in no_dupl for k, v in d.items()}
+
+        return new_dict
 
     @staticmethod
     def format_list(obj: list) -> set:
@@ -139,21 +175,20 @@ def main():
     output_folder = Path(args.output_folder)
     output_folder.mkdir(parents=True, exist_ok=True)
 
-    usr_list = []
+    keys = []
     for file in input_folder.glob('*.json'):
         with open(file, encoding="utf8") as f:
             print(f'Extracting file {file}')
             data = json.load(f)
             parser = ParseJson(data)
             res = parser.json_extract_usrs()
-            usr_list.append(res)
+            keys.append(res)
 
-    flat_usr = ParseJson.format_list(usr_list)
+    all_keys = ParseJson.format_dict(keys)
 
-    with open('myfile.csv', 'w', newline='\n') as csvfile:
-        csvwriter = csv.writer(csvfile,delimiter="\n")
-        csvwriter.writerow(['Username'])
-        csvwriter.writerow(flat_usr)
+    key_series = pd.Series(all_keys, name='Label')
+    key_series.to_csv('all_keys.csv',index_label='Value',header = True)
+
 
 if __name__ == '__main__':
     main()
