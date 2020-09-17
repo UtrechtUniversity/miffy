b6939cb604c85d9bb06df10044b489fd2e38b3d3
vos00076 <m.g.devos@uu.nl>
2020-09-17 09:34:18 +0200

    Finalize to resemble functionality of create_keys script

--- parse_json.py
+++ parse_json.py
@@ -13,28 +13,47 @@ import re
 class ParseJson:
     """ Extract usernames in jsonfiles"""
 
-    def __init__(self, data: dict,input_folder: Path,output_folder: Path):
+    def __init__(self, input_folder: Path, output_folder: Path):
         self.logger = logging.getLogger('anonymizing.parse_json')
         self.email = r'[\w\.-]+@[\w\.-]+'
-        self.data = data
         self.input_folder = input_folder
         self.output_folder = output_folder
         self.labels = self.get_labels()
 
-    def create_keys(self) -> dict:
+    def create_keys(self):
         """Extract user names from nested JSON."""
 
-        key_dict = {}
+        # Create key file
+        self.logger.info(f"Creating key file for {self.input_folder}...")
 
-        # Extract values with recursive function
-        keys = self.extract(self.data, key_dict)
+        keys = []
+        for file in self.input_folder.glob('*.json'):
+            # Per json file: extract sensitive info+labels and store in dict
+            with file.open(encoding="utf8") as f:
+                key_dict = {}
+                data = json.load(f)
+                res = self.extract(data,key_dict)
+                keys.append(res)
+
+        # Add common given names to dictionary
         common_names = self.common_names()
-        keys.update(common_names)
+        keys.append(common_names)
 
-        # Replace name label with hash code
-        hash_key_dict = {k:(self.mingle(v) if v == '__name' else v) for k, v in key_dict.items()}
+        # Combine in one dict; replace name labels with hash code
+        all_keys = ParseJson.format_dict(keys)
+        hash_key_dict = {k: (self.mingle(v) if v == '__name' else v) for k, v in all_keys.items()}
 
-        return hash_key_dict
+        # Replace info in profile.json
+        self.replace_profile()
+
+        # hash name of package owner in name output file
+        owner = str(self.input_folder.name).split('_')[0]
+        sub = hash_key_dict[owner]
+        outfile = self.output_folder / f'keys_{sub}.csv'
+
+        # write keys to csv file as input for anonymizeUU package
+        key_series = pd.Series(hash_key_dict, name='subt')
+        key_series.to_csv(outfile, index_label='id', header=True)
 
     def extract(self, obj, key_dict: dict) -> dict:
         """Recursively search for values of key in JSON tree."""
@@ -97,18 +116,18 @@ class ParseJson:
             if re.match(name, text):
                 return text
 
-    def check_phone(self,text: str):
+    def check_phone(self, text: str):
         """check if given string is valid phone nr"""
 
         patterns = [r'(?<!\d)\d{9,10}(?!\d)',
-                 r'[0-9]{2}\-[0-9]{8}']
+                    r'[0-9]{2}\-[0-9]{8}']
 
         phone_nrs = [re.compile(p) for p in patterns]
 
         if any(nr.match(text) for nr in phone_nrs):
             return text
 
-    def check_datetime(self,text: str) -> datetime:
+    def check_datetime(self, text: str) -> datetime:
         """Check if given string can be converted to a datetime format"""
         try:
             res = dateutil.parser.parse(text)
@@ -121,7 +140,7 @@ class ParseJson:
         except ValueError:
             pass
 
-    def get_username(self,obj: list):
+    def get_username(self, obj: list):
         """Check if given list contains username"""
 
         matches = [x for x in obj if self.check_datetime(x)]
@@ -139,8 +158,8 @@ class ParseJson:
 
         return usr_list
 
-    def common_names(self)-> dict:
-        """Add common given names in NL to keys dictionary"""
+    def common_names(self) -> dict:
+        """Add common given names in NL to keys dictionary; these may occur in free text like messages"""
 
         name_file = Path.cwd() / 'Firstnames_NL.lst'
         with name_file.open() as f:
@@ -154,8 +173,35 @@ class ParseJson:
 
         return name_dict
 
+    def replace_profile(self):
+        """Replace sensitive info in profile.json that Anonymize can't replace """
+
+        try:
+            json_file = self.input_folder / 'profile.json'
+            with json_file.open(encoding="utf8") as f:
+                data = json.load(f)
+
+            file = json.dumps(data)
+
+            # Replace bio and gender info
+            bio = re.findall(re.compile("biography\": \"(.*?)\""), file)
+            gender = re.findall(re.compile("gender\": \"(.*?)\""), file)
+
+            if len(bio) >= 1:
+                file = file.replace(bio[0], '__bio')
+            if len(gender) >= 1:
+                file = file.replace(gender[0], '__gender')
+
+            # Save files
+            df = json.loads(file)
+            with json_file.open('w', encoding="utf8") as outfile:
+                json.dump(df, outfile)
+
+        except FileNotFoundError:
+            pass
+
     @staticmethod
-    def mingle(text:str)-> str:
+    def mingle(text: str) -> str:
         """ Creates scrambled version with letters and numbers of entered word """
 
         if len(str(text)) > 1:
@@ -166,7 +212,7 @@ class ParseJson:
         return pseudo
 
     @staticmethod
-    def format_dict(obj:list)-> dict:
+    def format_dict(obj: list) -> dict:
         """Format irregular list of dictionaries and remove duplicates"""
 
         no_dupl = [i for n, i in enumerate(obj) if i not in obj[n + 1:]]
@@ -208,19 +254,8 @@ def main():
     output_folder = Path(args.output_folder)
     output_folder.mkdir(parents=True, exist_ok=True)
 
-    keys = []
-    for file in input_folder.glob('*.json'):
-        with file.open(encoding="utf8") as f:
-            print(f'Extracting file {file}')
-            data = json.load(f)
-            parser = ParseJson(data,input_folder,output_folder)
-            res = parser.create_keys()
-            keys.append(res)
-
-    all_keys = ParseJson.format_dict(keys)
-
-    key_series = pd.Series(all_keys, name='Label')
-    key_series.to_csv('all_keys.csv',index_label='Value',header = True)
+    parser = ParseJson(input_folder, output_folder)
+    parser.create_keys()
 
 
 if __name__ == '__main__':
